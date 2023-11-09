64c8b878585e89498f69042fe74878424acdce76
Alex Teper <67521917+AlexTeper99@users.noreply.github.com>
2023-11-09 12:11:44 -0300

    test-arc: handleErrors

---
+++ src/midldlewares/handleErrors.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+
+export const handleErrors = (
+  error: any,
+  _request: Request,
+  response: Response,
+  _next: NextFunction
+) => {
+  if (error.name === "CastError") {
+    console.log("Cast error");
+    response.status(400).send({ error: "id used is malformed" });
+  } else if (error.name === "ValidationError") {
+    response.status(409).send({
+      error: error.message,
+    });
+  } else {
+    response.status(500).send({ error: error.message });
+  }
+};
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,16 @@
 import express from "express";
+import { handleErrors } from "../midldlewares";
 
 export const router = express.Router();
 
-router.get("/", (_req, res) => {
-  res.send("Hello World from Users");
+router.get("/", (_req, _res, next) => {
+  setTimeout(() => {
+    try {
+      throw new Error("BROKEN");
+    } catch (err) {
+      next(err);
+    }
+  }, 0);
 });
+
+router.use(handleErrors);
